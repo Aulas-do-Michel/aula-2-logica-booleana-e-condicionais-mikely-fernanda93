e4fc396d4828539038db615781cda01923c7db65
Mikely Fernanda <163673682+mikely-fernanda93@users.noreply.github.com>
2024-03-23 21:39:53 -0300

    Update exercicio_3.py

--- exercicio_3.py
+++ exercicio_3.py
@@ -55,3 +55,41 @@ Resposta:
 Não
 
 """
+
+"""
+#### Exercício 3 - Identificar se a variante está no gene BRCA1 - Versão 2.
+
+Receba 3 inputs do usuário:
+1) O cromossomo de uma variante. Ele virá escrito como texto e da seguinte forma "chr1", "chr2", etc.
+2) A posição dessa variante. Será um número inteiro.
+3) O genoma de referência dessa variante. Considere só 2 opções possíveis, "hg19" e "hg38".
+
+Responda:
+"Sim" se ela estiver no BRCA1.
+"Não" se ela não estiver.
+
+Considere que a variante está no gene BRCA1 se estiver no cromossomo 17 (chr17), e:
+1) Se a posição estiver no intevalo de 41196312 a 41277500, caso o genoma de referência seja o "hg19".
+2) Se a posição estiver no intevalo de 43044295 a 43125483, caso o genoma de referência seja o "hg38".
+
+Obs: Tirei a Location daqui: 
+https://grch37.ensembl.org/Homo_sapiens/Gene/Summary?g=ENSG00000012048;r=17:41196312-41277500
+"""
+
+
+cromossomo = input('Digite o cromossomo (chrX) de uma variante: ')
+posicao = int(input('Digite a posição dessa variante (número inteiro): '))
+genomaRef = input('Escolha o genoma de referencia hg19 OU hg38: ')
+
+
+if cromossomo == 'chr17':
+    if genomaRef == 'hg19' and (posicao >= 41196312 and posicao <= 41277500):
+        print('Sim')
+    elif genomaRef == 'hg38'and (posicao >= 43044295 and posicao <= 43125483):
+        print('Sim')
+    else:
+        print('Não')
+else:
+    print('Não')
+
+
