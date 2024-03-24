7a017e60373145ed1300e49afe6af2d58c057b03
Mikely Fernanda <163673682+mikely-fernanda93@users.noreply.github.com>
2024-03-23 21:05:13 -0300

    Update exercicio_2.py

--- exercicio_2.py
+++ exercicio_2.py
@@ -37,3 +37,31 @@ Resposta:
 Sim
 
 """
+
+"""
+#### Exercício 2 =>
+Identificar se a variante está no gene BRCA1 - Versão 1.
+
+Receba 2 inputs do usuário:
+1) O cromossomo de uma variante. Ele virá escrito como texto e da seguinte forma "chr1", "chr2", etc.
+2) A posição dessa variante. Será um número inteiro.
+
+Responde:
+"Sim" se ela estiver no BRCA1.
+"Não" se ela não estiver.
+
+Considere que a variante está no gene BRCA1 se estiver no cromossomo 17 (chr17), e se a posição estiver 
+no intevalo de 41196312 a 41277500.
+Obs: Tirei a localização daqui: 
+https://grch37.ensembl.org/Homo_sapiens/Gene/Summary?g=ENSG00000012048;r=17:41196312-41277500.
+"""
+
+cromossomo = input('Digite o cromossomo (chrX) de uma variante: ')
+posicao = int(input('Digite a posição dessa variante (número inteiro): '))
+
+if cromossomo == 'chr17' and (posicao >= 41196312 and posicao <= 41277500):
+    print('Sim')
+else:
+    print('Não')
+
+
